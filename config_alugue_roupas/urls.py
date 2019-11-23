8399b32cff19268110d52f0fd16f3262d32c3768
gabrdgs <gabbiematiias@gmail.com>
2019-11-22 22:08:37 -0300

    adicionar forms em views

--- config_alugue_roupas/urls.py
+++ config_alugue_roupas/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('produtos/', views.produtos),
+    path('cadastro/', views.cadastro_pedido)
 ]
--- website/forms.py
+++ website/forms.py
@@ -11,5 +11,3 @@ class PedidoForm(forms.ModelForm):
             'cartao',
             'pagamento',
         ]
-
-
