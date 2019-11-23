0aad7834db7252701e3ba3c59af8eec3089f3a64
gabrdgs <gabbiematiias@gmail.com>
2019-11-22 21:19:32 -0300

    criar um form de Pedido

---
+++ website/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from website.models import Pedido
+
+
+class PedidoForm(forms.ModelForm):
+    class Meta:
+        model = Pedido
+        fields = [
+            'nome',
+            'email',
+            'cartao',
+            'pagamento',
+        ]
+
+
